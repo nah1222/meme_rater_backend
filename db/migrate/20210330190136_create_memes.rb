a9caaa67be913da93159dfef5e3708070abdb21d
nah1222 <nah1222@yahoo.com>
2021-03-30 15:03:50 -0400

    models

---
+++ db/migrate/20210330190136_create_memes.rb
@@ -0,0 +1,11 @@
+class CreateMemes < ActiveRecord::Migration[6.0]
+  def change
+    create_table :memes do |t|
+      t.string :name
+      t.string :image_url
+      t.string :slug
+
+      t.timestamps
+    end
+  end
+end
